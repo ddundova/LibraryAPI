4cabfe2b31e4f37bba48f992261330063a1a47eb
dimid <dimid@LAPTOP-ATFI0E9H>
2025-06-12 19:11:55 +0300

    Category Controller

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,80 @@
+﻿using LibraryAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        LibraryContext _context = new LibraryContext();
+
+        public CategoryController(LibraryContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult getAll()
+        {
+            var result = _context.Categories.ToList();
+            return Ok(result);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult getById([FromRoute]int id)
+        {
+            var category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category == null)
+            {
+                return NotFound($"No category found with id = {id}!");
+            }
+            return Ok(category);
+        }
+
+        [HttpPost]
+        public IActionResult addCategory([FromBody] string name)
+        {
+            Category? newCategory = _context.Categories.FirstOrDefault(c => c.Name == name);
+
+            if (newCategory != null)
+            {
+                return BadRequest($"The category {name} already exists!");
+            }
+
+            _context.Categories.Add(newCategory);
+            _context.SaveChanges();
+            return Ok("Category has been added!");
+        }
+
+        [HttpPut]
+        public IActionResult updateCategory([FromBody] int id, string newName)
+        {
+            Category? updatedCategory = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (updatedCategory != null)
+            {
+                updatedCategory.Name = newName;
+
+                _context.SaveChanges();
+
+                return Ok($"Category with id = {id} has been successfully updated!");
+            }
+
+            return NotFound($"Vehicle with id = {id} doesn't exist!");
+        }
+
+        [HttpDelete]
+        public IActionResult deleteCategory([FromBody] int id) 
+        { 
+            Category? category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if (category != null)
+            {
+                _context.Categories.Remove(category);
+                _context.SaveChanges();
+                return Ok($"Category with id = {id} has been successfully deleted!");
+            }
+            return NotFound($"Category with id = {id} doesn't exist!");
+        }
+    }
+}
--- Models/LibraryContext.cs
+++ Models/LibraryContext.cs
@@ -21,10 +21,6 @@ public partial class LibraryContext : DbContext
 
     public virtual DbSet<Category> Categories { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=.;Database=Library;Trusted_Connection=True;TrustServerCertificate=True;");
-
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         modelBuilder.Entity<Author>(entity =>
